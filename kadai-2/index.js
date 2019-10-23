3216168f40e1cd758e6b72f7d2827ecd397646c7
Negami Kazushige <avaritsumei.kazushige.n@gmail.com>
2019-10-23 05:16:06 +0000

    some fix

---
+++ kadai-2/index.js
@@ -0,0 +1,14 @@
+const firstName = "Kazushige"
+const secondName = "Negami"
+console.log(firstName+secondName)
+
+const sum = 1+2+3+4+5+6+7+8+9+10
+console.log(sum)
+
+const hasDog = true
+const hasCat = false
+
+console.log(hasDog||hasCat)
+console.log(hasDog&&hasCat)
+
+console.log(!hasDog&&!hasCat)
