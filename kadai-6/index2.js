a2e268482a6930752d66480161e7ce6d679de6c5
Negami Kazushige <avaritsumei.kazushige.n@gmail.com>
2019-12-11 05:17:21 +0000

    課題6を提出

---
+++ kadai-6/index2.js
@@ -0,0 +1,24 @@
+const str1 = "abc"
+const str2 = "'abc'"
+const str3 = '"abc"'
+const str4 = '{"a": undefined}'
+const str5 = '{"a": 123}'
+const str6 = '{a: 123}'
+
+try {
+    console.log(JSON.parse(str1))
+} catch (error) {
+    console.log (error)
+}
+
+try {
+    console.log(JSON.parse(str2))
+} catch (error) {
+    console.log (error)
+}
+
+try {
+    console.log(JSON.parse(str3))
+} catch (error) {
+    console.log (error)
+}
